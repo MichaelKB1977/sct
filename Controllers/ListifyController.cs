b609bce73afc4409c413a7c61a8841ca9fb3d8a1
Michael <michaelkb@hotmail.com>
2021-02-04 12:28:03 -0600

    Add project files.

---
+++ Controllers/ListifyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShortCodeTestAzureRepo.Controllers
+{
+    [Route("listify")]
+    [ApiController]
+    public class ListifyController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{startRange?}/{endRange?}/{requestedIndex?}")]
+        public ActionResult<IList<int>> Get(int? startRange, int? endRange, int? requestedIndex)
+        {
+            if(startRange == null)
+            {
+                throw new ArgumentNullException(nameof(startRange));
+            }
+            else if (startRange == null)
+            {
+                throw new ArgumentNullException(nameof(endRange));
+            }
+            if (startRange == null)
+            {
+                throw new ArgumentNullException(nameof(requestedIndex));
+            }
+            else
+                return Enumerable.Range((int)startRange, (int)endRange).Skip((int)requestedIndex).Take(1).ToList();
+        }
+    }
+}
